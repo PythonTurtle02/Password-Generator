58ac6d37e4257dc19219a5bcbf11899376bae76c
Python-Turtle <133562016+PythonTurtle02@users.noreply.github.com>
2023-07-16 16:03:01 +0530

    password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -9,9 +9,3 @@ combination = lower_case + upper_case + numbers + symbols
 length_of_password = 8
 password = "".join(random.sample(combination, length_of_password))
 print("The Generated Password is :", password)
-
-
-
-
-
-
