e05ba76b6d4c5f13486d6a1722b6245baefa9b5e
Python-Turtle <133562016+PythonTurtle02@users.noreply.github.com>
2023-07-16 16:01:21 +0530

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,17 @@
+import random
+
+lower_case = "abcdefghijklmnopqrstuvwxyz"
+upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "0123456789"
+symbols = "@#$%&*?"
+
+combination = lower_case + upper_case + numbers + symbols
+length_of_password = 8
+password = "".join(random.sample(combination, length_of_password))
+print("The Generated Password is :", password)
+
+
+
+
+
+
